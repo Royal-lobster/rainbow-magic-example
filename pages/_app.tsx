ec4067f406b3124736a9ad5f61835f03000b033b
Srujan Gurram <srujangs8@gmail.com>
2023-05-02 22:22:22 +0530

    remove next cache from git

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,49 +3,49 @@ import type { AppProps } from "next/app";
 
 import "@rainbow-me/rainbowkit/styles.css";
 import {
-  RainbowKitProvider,
-  darkTheme,
-  connectorsForWallets,
+	RainbowKitProvider,
+	darkTheme,
+	connectorsForWallets,
 } from "@rainbow-me/rainbowkit";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
 import { rainbowMagicConnector } from "../lib/RainbowMagicConnector";
 import {
-  arbitrum,
-  goerli,
-  hardhat,
-  mainnet,
-  optimism,
-  polygon,
+	arbitrum,
+	goerli,
+	hardhat,
+	mainnet,
+	optimism,
+	polygon,
 } from "@wagmi/chains";
 
 const { chains, provider } = configureChains(
-  [mainnet, goerli, polygon, optimism, arbitrum, hardhat],
-  [
-    // alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
-    publicProvider(),
-  ]
+	[mainnet, goerli, polygon, optimism, arbitrum, hardhat],
+	[
+		// alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
+		publicProvider(),
+	],
 );
 
 const connectors = connectorsForWallets([
-  {
-    groupName: "Recommended",
-    wallets: [rainbowMagicConnector({ chains })],
-  },
+	{
+		groupName: "Recommended",
+		wallets: [rainbowMagicConnector({ chains })],
+	},
 ]);
 
 const wagmiClient = createClient({
-  autoConnect: false,
-  connectors,
-  provider,
+	autoConnect: false,
+	connectors,
+	provider,
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider theme={darkTheme()} chains={chains}>
-        <Component {...pageProps} />
-      </RainbowKitProvider>
-    </WagmiConfig>
-  );
+	return (
+		<WagmiConfig client={wagmiClient}>
+			<RainbowKitProvider theme={darkTheme()} chains={chains}>
+				<Component {...pageProps} />
+			</RainbowKitProvider>
+		</WagmiConfig>
+	);
 }
